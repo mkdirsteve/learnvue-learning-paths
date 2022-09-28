2fd3ec5cec0e32c16c7e961b22b1a01e6df65247
Matt Maribojoc <matthewmaribojoc@gmail.com>
2022-09-27 23:56:04 -0400

    auto animate demo

---
+++ astro/src/tutorials/autoanimate/Demo.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { vAutoAnimate } from '@formkit/auto-animate'
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+</script>
+
+<template>
+  <div v-auto-animate="{ duration: 200 }" class="p-8 rounded-lg bg-neutral-900">
+    <h2 @click="isOpen = !isOpen" class="text-xl font-bold">
+      Why you should subscribe
+    </h2>
+    <p v-if="isOpen" class="py-8">I'm close to 20k so please 🙏</p>
+  </div>
+</template>
---
+++ vue-vite/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const items = ref(['😏', '😐', '😑', '😒', '😕'])
+function removeItem(toRemove: any) {
+  items.value = items.value.filter((item) => item !== toRemove)
+}
+</script>
+
+<template>
+  <div>
+    <h5>Click emojis to remove them.</h5>
+    <ul v-auto-animate>
+      <li v-for="item in items" :key="item" @click="removeItem(item)">
+        {{ item }}
+      </li>
+    </ul>
+  </div>
+</template>
